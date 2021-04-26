ba1440678de5d74fdb386d631b79c7e40ff40bdc
Stephanie Viste <srviste@mymail.mapua.edu.ph>
2021-04-26 11:40:22 +0800

    another update on the project: -added definitions at the backend -not yet finish

--- CCTDdb_BackEnd.py
+++ CCTDdb_BackEnd.py
@@ -1,1 +1,39 @@
+#backend
+import sqlite3
+def PatientData():
+    con=sqlite3.connect("patient.db")
+    cur.execute("CREATE TABLE IF NOT EXISTS patient(id INTEGER PRIMARY KEY, PntID text, Firstname text, Surname text, DoB text,\
+    Age text, Gender text, Address text, Mobile text, LastPersonContact text)")
+    con.commit()
+    con.clone()
 
+def AddPntRec(PntID, Firstname, Surname, DoB, Age, Gender, Address, Mobile, LastPersonContact):
+     con=sqlite3.connect("patient.db")
+     cur = con.cursor()
+     cur.execute("INSERT INTO patient VALUES(NULL, ?,?,?,?,?,?,?,?,?,)",PntID, Firstname, Surname, DoB, Age, Gender, Address, Mobile, LastPersonContact)
+     con.commit()
+     con.close()
+
+def viewData():
+    con=sqlite3.connect("patient.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM patient")
+    row = cur.fetchall()
+    con.commit()
+    con.close()
+    return rows
+def deleteRec():
+    con=sqlite3.connect("patient.db")
+    cur = con.cursor()
+    cur.execute("DELETE FROM patient WHERE id=?", (id,))
+    con.commit()
+    con.close()
+def searchData(PntID="", Firstname="", Surname="", DoB="", Age="", Gender="", Address="",Mobile="", LastPersonContact=""):
+    con=sqlite3.connect("patient.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM patient WHERE Pntid=? OR Firstname=? OR Surname=? OR DoB=? OR Age=? OR Gender=? OR Address=? \
+    OR Mobile=? OR LastPersonContact=?", (PntID, Firstname, Surname, DoB, Age, Gender, Address, Mobile, LastPersonContact,id))
+    con.commit()
+    con.close()
+
+PatientData()
