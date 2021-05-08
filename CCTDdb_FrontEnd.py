db2ff75bca44f66744e2f298fa84d6e443741fb1
Stephanie Viste <srviste@mymail.mapua.edu.ph>
2021-05-08 19:08:54 +0800

    revise on design

--- CCTDdb_FrontEnd.py
+++ CCTDdb_FrontEnd.py
@@ -100,101 +100,110 @@ class Patient:
         MainFrame = Frame(self.root, bg="cadet blue")
         MainFrame.grid()
 
-        TitFrame = Frame(MainFrame, bd=2, padx=54, pady=8, bg="Ghost White", relief=RIDGE)
-        TitFrame.pack(side=TOP)
+        #TitFrame = Frame(MainFrame, bd=2, padx=54, pady=8, bg="Ghost White", relief=RIDGE)
+        #TitFrame.pack(side=TOP)
 
-        self.lblTit = Label(TitFrame, font=('arial', 47,'bold'),text="CCTD Management Systems", bg="Ghost White")
-        self.lblTit.grid()
+        #self.lblTit = Label(TitFrame, font=('arial', 47,'bold'),text="CCTD Management Systems", bg="Ghost White")
+        #self.lblTit.grid()
+        
+        DataFrame2 = Frame(MainFrame, bd=1, width=1300, height=400, padx=20, pady=20, relief=RIDGE, bg="cadet blue")
+        DataFrame2.pack(side=BOTTOM)
 
-        ButtonFrame = Frame(MainFrame, bd=2, width=1350, height=70, padx=18, pady=10, bg="Ghost White", relief=RIDGE)
+        ListFrame = LabelFrame(DataFrame2, bd=2, width=1350, height=100, padx=18, pady=10, relief=RIDGE, bg="Ghost White",
+                                    font=('Arial', 20,'bold'), text="Patient List Details\n")
+        ListFrame.pack(side=TOP)
+        
+        ButtonFrame = Frame(DataFrame2, bd=2, width=1350, height=70, padx=18, pady=10, bg="Ghost White", relief=RIDGE)
         ButtonFrame.pack(side=BOTTOM)
         
         DataFrame = Frame(MainFrame, bd=1, width=1300, height=400, padx=20, pady=20, relief=RIDGE, bg="cadet blue")
         DataFrame.pack(side=BOTTOM)
 
-        DataFrameLEFT = LabelFrame(DataFrame, bd=1, width=1000, height=600, padx=20, relief=RIDGE, bg="Ghost White",
-                                   font=('arial', 20,'bold'), text="Patient Info\n")
+        DataFrameLEFT = LabelFrame(DataFrame, bd=1, width=900, height=400, padx=20, relief=RIDGE, bg="Ghost White",
+                                   font=('Arial', 20,'bold'), text="Patient Info\n")
         DataFrameLEFT.pack(side=LEFT)
 
-        DataFrameRIGHT = LabelFrame(DataFrame, bd=1, width=450, height=300, padx=31, pady=3, relief=RIDGE, bg="Ghost White",
-                                    font=('arial', 20,'bold'), text="Patient List Details\n")
+        DataFrameRIGHT = LabelFrame(DataFrame, bd=1, width=450, height=400, padx=31, pady = 3, relief=RIDGE, bg="Ghost White",
+                                   font=('Arial', 20,'bold'), text="Records\n")
         DataFrameRIGHT.pack(side=RIGHT)
 
+       
+
         #==================Labels and Entry Widget==================
-        self.lblPntID = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Patient ID:",padx=2, pady=2,bg="Ghost White")
+        self.lblPntID = Label(DataFrameLEFT, font=('Arial', 20, 'bold'), text="Patient ID:",padx=2, pady=2,bg="Ghost White")
         self.lblPntID.grid(row=0, column=0, sticky=W)
-        self.txtPntID = Entry(DataFrameLEFT, font=('arial', 20, 'bold'), textvariable=PntID,width=39)
+        self.txtPntID = Entry(DataFrameLEFT, font=('Arial', 20, 'bold'), textvariable=PntID,width=39)
         self.txtPntID.grid(row=0, column=1)
 
-        self.lblfna = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="First Name:",padx=2, pady=2,bg="Ghost White")
+        self.lblfna = Label(DataFrameLEFT, font=('Arial', 20, 'bold'), text="First Name:",padx=2, pady=2,bg="Ghost White")
         self.lblfna.grid(row=1, column=0, sticky=W)
-        self.txtfna = Entry(DataFrameLEFT, font=('arial', 20, 'bold'), textvariable=Firstname,width=39)
+        self.txtfna = Entry(DataFrameLEFT, font=('Arial', 20, 'bold'), textvariable=Firstname,width=39)
         self.txtfna.grid(row=1, column=1)
 
-        self.lblSna = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Surname:",padx=2, pady=2,bg="Ghost White")
+        self.lblSna = Label(DataFrameLEFT, font=('Arial', 20, 'bold'), text="Surname:",padx=2, pady=2,bg="Ghost White")
         self.lblSna.grid(row=2, column=0, sticky=W)
-        self.txtSna = Entry(DataFrameLEFT, font=('arial', 20, 'bold'), textvariable=Surname,width=39)
+        self.txtSna = Entry(DataFrameLEFT, font=('Arial', 20, 'bold'), textvariable=Surname,width=39)
         self.txtSna.grid(row=2, column=1)
 
-        self.lblDob = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Date of Birth:",padx=2, pady=2,bg="Ghost White")
+        self.lblDob = Label(DataFrameLEFT, font=('Arial', 20, 'bold'), text="Date of Birth:",padx=2, pady=2,bg="Ghost White")
         self.lblDob.grid(row=3, column=0, sticky=W)
-        self.txtDob = Entry(DataFrameLEFT, font=('arial', 20, 'bold'), textvariable=DoB,width=39)
+        self.txtDob = Entry(DataFrameLEFT, font=('Arial', 20, 'bold'), textvariable=DoB,width=39)
         self.txtDob.grid(row=3, column=1)
 
-        self.lblAge = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Age:",padx=2, pady=2,bg="Ghost White")
+        self.lblAge = Label(DataFrameLEFT, font=('Arial', 20, 'bold'), text="Age:",padx=2, pady=2,bg="Ghost White")
         self.lblAge.grid(row=4, column=0, sticky=W)
-        self.txtAge = Entry(DataFrameLEFT, font=('arial', 20, 'bold'), textvariable=Age,width=39)
+        self.txtAge = Entry(DataFrameLEFT, font=('Arial', 20, 'bold'), textvariable=Age,width=39)
         self.txtAge.grid(row=4, column=1)
 
-        self.lblGender = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Gender:",padx=2, pady=2,bg="Ghost White")
+        self.lblGender = Label(DataFrameLEFT, font=('Arial', 20, 'bold'), text="Gender:",padx=2, pady=2,bg="Ghost White")
         self.lblGender.grid(row=5, column=0, sticky=W)
-        self.txtGender = Entry(DataFrameLEFT, font=('arial', 20, 'bold'), textvariable=Gender,width=39)
+        self.txtGender = Entry(DataFrameLEFT, font=('Arial', 20, 'bold'), textvariable=Gender,width=39)
         self.txtGender.grid(row=5, column=1)
 
-        self.lblAdr = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Address:",padx=2, pady=2,bg="Ghost White")
+        self.lblAdr = Label(DataFrameLEFT, font=('Arial', 20, 'bold'), text="Address:",padx=2, pady=2,bg="Ghost White")
         self.lblAdr.grid(row=6, column=0, sticky=W)
-        self.txtAdr = Entry(DataFrameLEFT, font=('arial', 20, 'bold'), textvariable=Address,width=39)
+        self.txtAdr = Entry(DataFrameLEFT, font=('Arial', 20, 'bold'), textvariable=Address,width=39)
         self.txtAdr.grid(row=6, column=1)
 
-        self.lblMoNo = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Mobile No:",padx=2, pady=2,bg="Ghost White")
+        self.lblMoNo = Label(DataFrameLEFT, font=('Arial', 20, 'bold'), text="Mobile No:",padx=2, pady=2,bg="Ghost White")
         self.lblMoNo.grid(row=7, column=0, sticky=W)
-        self.txtMoNo = Entry(DataFrameLEFT, font=('arial', 20, 'bold'), textvariable=MoNo,width=39)
+        self.txtMoNo = Entry(DataFrameLEFT, font=('Arial', 20, 'bold'), textvariable=MoNo,width=39)
         self.txtMoNo.grid(row=7, column=1)
 
-        self.lblLastPC = Label(DataFrameLEFT, font=('arial', 20, 'bold'), text="Last Person Contact:",padx=2, pady=2,bg="Ghost White")
+        self.lblLastPC = Label(DataFrameLEFT, font=('Arial', 20, 'bold'), text="Last Person Contact:",padx=2, pady=2,bg="Ghost White")
         self.lblLastPC.grid(row=8, column=0, sticky=W)
-        self.txtLastPC = Entry(DataFrameLEFT, font=('arial', 20, 'bold'), textvariable=LastPersonContact,width=39)
+        self.txtLastPC = Entry(DataFrameLEFT, font=('Arial', 20, 'bold'), textvariable=LastPersonContact,width=39)
         self.txtLastPC.grid(row=8, column=1)
 
         #===================================ListBox & ScrollBar Widget==================================
-        scrollbar = Scrollbar(DataFrameRIGHT)
+        scrollbar = Scrollbar(ListFrame)
         scrollbar.grid(row=0, column=1, sticky='ns')
 
-        Patientlist = Listbox(DataFrameRIGHT, width=41, height=16, font=('arial', 12, 'bold'), yscrollcommand=scrollbar.set)
+        Patientlist = Listbox(ListFrame, width=141, height=7, font=('Arial', 12, 'bold'), yscrollcommand=scrollbar.set)
         Patientlist.bind('<<ListboxSelect>>', PatientRec)
         Patientlist.grid(row=0, column=0, padx=9)
         scrollbar.config(command = Patientlist.yview)
 
         #===================================Button Widget==================================
-        self.btnAddData = Button(ButtonFrame, text="Add New",font=('arial', 20, 'bold'), height=1,width=10,bd=4, command=addData)
+        self.btnAddData = Button(ButtonFrame, text="Add New",font=('Arial', 20, 'bold'), height=1,width=10,bd=4, command=addData)
         self.btnAddData.grid(row=0, column=0)
 
-        self.btnDisplayData = Button(ButtonFrame, text="Display",font=('arial', 20, 'bold'), height=1,width=10,bd=4, command=displayData)
+        self.btnDisplayData = Button(ButtonFrame, text="Display",font=('Arial', 20, 'bold'), height=1,width=10,bd=4, command=displayData)
         self.btnDisplayData.grid(row=0, column=1)
 
-        self.btnClearData = Button(ButtonFrame, text="Clear",font=('arial', 20, 'bold'), height=1,width=10,bd=4, command=clearData)
+        self.btnClearData = Button(ButtonFrame, text="Clear",font=('Arial', 20, 'bold'), height=1,width=10,bd=4, command=clearData)
         self.btnClearData.grid(row=0, column=2)
 
-        self.btnDeleteData = Button(ButtonFrame, text="Delete",font=('arial', 20, 'bold'), height=1,width=10,bd=4, command=deleteData)
+        self.btnDeleteData = Button(ButtonFrame, text="Delete",font=('Arial', 20, 'bold'), height=1,width=10,bd=4, command=deleteData)
         self.btnDeleteData.grid(row=0, column=3)
 
-        self.btnSearchData = Button(ButtonFrame, text="Search",font=('arial', 20, 'bold'), height=1,width=10,bd=4, command=SearchData)
+        self.btnSearchData = Button(ButtonFrame, text="Search",font=('Arial', 20, 'bold'), height=1,width=10,bd=4, command=SearchData)
         self.btnSearchData.grid(row=0, column=4)
 
-        self.btnUpdateData = Button(ButtonFrame, text="Update",font=('arial', 20, 'bold'), height=1,width=10,bd=4, command=update)
+        self.btnUpdateData = Button(ButtonFrame, text="Update",font=('Arial', 20, 'bold'), height=1,width=10,bd=4, command=update)
         self.btnUpdateData.grid(row=0, column=5)
 
-        self.btnExit = Button(ButtonFrame, text="Exit",font=('arial', 20, 'bold'), height=1,width=10,bd=4, command = iExit)
+        self.btnExit = Button(ButtonFrame, text="Exit",font=('Arial', 20, 'bold'), height=1,width=10,bd=4, command = iExit)
         self.btnExit.grid(row=0, column=6)
         
            
